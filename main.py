917fce636841dbff679bc868425d02c9c3fd9cf7
MaybeCaat <iemm@rambler.ru>
2022-10-22 08:54:41 +0300

    create bot template

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+# стратегии - если понадобятся
+# from aiogram.fsm.strategy import FSMStrategy
+
+# прочтение конфига
+from config_reader import config
+from handlers import common, ordering_food
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(name)s - %(message)s \n---------------------------------------------------"
+    )
+
+    # создаём Dispatcher и Bot
+    dp = Dispatcher(storage=MemoryStorage())
+    bot = Bot(config.bot_token.get_secret_value())
+
+    dp.include_router(common.router)
+    dp.include_router(ordering_food.router)
+
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
 
 if __name__ == '__main__':
-    print('Hello World')
-    #asyncio.run(main())
+    asyncio.run(main())
